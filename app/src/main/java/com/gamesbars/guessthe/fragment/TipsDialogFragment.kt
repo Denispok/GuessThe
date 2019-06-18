f006c79410e709b83e212a1a3bbeff0c93b6cb62
Shakhov <shakhover@yandex.ru>
2019-06-18 12:43:39 +0300

    fix sound bug and remove useless coroutine

--- app/src/main/java/com/gamesbars/guessthe/fragment/TipsDialogFragment.kt
+++ app/src/main/java/com/gamesbars/guessthe/fragment/TipsDialogFragment.kt
@@ -21,77 +21,76 @@ class TipsDialogFragment : DialogFragment() {
         val builder = AlertDialog.Builder(context)
         val view = activity!!.layoutInflater.inflate(R.layout.dialog_tips, null)
 
-        CoroutineScope(Dispatchers.Main).launch {
-            val fragment = fragmentManager!!.findFragmentByTag(resources.getString(R.string.level_fragment_tag)) as LevelFragment
+        val fragment = fragmentManager!!.findFragmentByTag(resources.getString(R.string.level_fragment_tag)) as LevelFragment
 
-            val tipLetter = view.findViewById<RelativeLayout>(R.id.tips_letter)
-            val tipRemove = view.findViewById<RelativeLayout>(R.id.tips_remove)
-            val tipSkip = view.findViewById<RelativeLayout>(R.id.tips_skip)
-            view.findViewById<TextView>(R.id.tips_cancel).setOnClickListener {
-                playSound(context!!, R.raw.button)
+        val tipLetter = view.findViewById<RelativeLayout>(R.id.tips_letter)
+        val tipRemove = view.findViewById<RelativeLayout>(R.id.tips_remove)
+        val tipSkip = view.findViewById<RelativeLayout>(R.id.tips_skip)
+        view.findViewById<TextView>(R.id.tips_cancel).setOnClickListener {
+            playSound(context!!, R.raw.button)
+            dismiss()
+            updateFragment(fragment)
+        }
+
+        val tipLetterCost = resources.getInteger(R.integer.tip_letter_cost)
+        val tipRemoveCost = resources.getInteger(R.integer.tip_remove_cost)
+        val tipSkipCost = resources.getInteger(R.integer.tip_skip_cost)
+
+        view.findViewById<TextView>(R.id.tips_letter_cost).text = tipLetterCost.toString()
+        view.findViewById<TextView>(R.id.tips_remove_cost).text = tipRemoveCost.toString()
+        view.findViewById<TextView>(R.id.tips_skip_cost).text = tipSkipCost.toString()
+
+        val saves = context!!.getSharedPreferences("saves", Context.MODE_PRIVATE)
+        val level = saves.getInt(fragment.pack, 0)
+        val levelName = fragment.pack + level
+        var levelString = saves.getString(levelName, "")
+
+        val coins = saves.getInt("coins", 0)
+        if (coins >= tipLetterCost) {
+            tipLetter.setOnClickListener {
+                val letter = fragment.letters.tipShowLetter(fragment.wordLetters)
+
+                val replacedString = "_" + letter.letter + letter.wordLetterId!!.toString()
+                val newString = "_" + letter.letter + letter.wordLetterId.toString() + "*"
+                levelString = levelString.replace(replacedString, newString)
+
+                val editor = saves.edit()
+                editor.putString(levelName, levelString)
+                editor.putInt("coins", coins - tipLetterCost)
+                editor.apply()
+
+                playSound(context!!, R.raw.tips)
                 dismiss()
                 updateFragment(fragment)
             }
+        } else tipLetter.isEnabled = false
+        if (coins >= tipRemoveCost && levelString.last() != '!') {
+            tipRemove.setOnClickListener {
+                fragment.letters.tipRemoveLetters(fragment.wordLetters)
 
-            val tipLetterCost = resources.getInteger(R.integer.tip_letter_cost)
-            val tipRemoveCost = resources.getInteger(R.integer.tip_remove_cost)
-            val tipSkipCost = resources.getInteger(R.integer.tip_skip_cost)
+                val editor = saves.edit()
+                editor.putString(levelName, "$levelString!")
+                editor.putInt("coins", coins - tipRemoveCost)
+                editor.apply()
 
-            view.findViewById<TextView>(R.id.tips_letter_cost).text = tipLetterCost.toString()
-            view.findViewById<TextView>(R.id.tips_remove_cost).text = tipRemoveCost.toString()
-            view.findViewById<TextView>(R.id.tips_skip_cost).text = tipSkipCost.toString()
+                playSound(context!!, R.raw.tips)
+                dismiss()
+                updateFragment(fragment)
+            }
+        } else tipRemove.isEnabled = false
+        if (coins >= tipSkipCost) {
+            tipSkip.setOnClickListener {
+                val editor = saves.edit()
+                editor.putInt("coins", coins - tipSkipCost)
+                editor.apply()
 
-            val saves = context!!.getSharedPreferences("saves", Context.MODE_PRIVATE)
-            val level = saves.getInt(fragment.pack, 0)
-            val levelName = fragment.pack + level
-            var levelString = saves.getString(levelName, "")
+                playSound(context!!, R.raw.tips)
+                dismiss()
+                updateFragment(fragment)
+                fragment.win()
+            }
+        } else tipSkip.isEnabled = false
 
-            val coins = saves.getInt("coins", 0)
-            if (coins >= tipLetterCost) {
-                tipLetter.setOnClickListener {
-                    val letter = fragment.letters.tipShowLetter(fragment.wordLetters)
-
-                    val replacedString = "_" + letter.letter + letter.wordLetterId!!.toString()
-                    val newString = "_" + letter.letter + letter.wordLetterId.toString() + "*"
-                    levelString = levelString.replace(replacedString, newString)
-
-                    val editor = saves.edit()
-                    editor.putString(levelName, levelString)
-                    editor.putInt("coins", coins - tipLetterCost)
-                    editor.apply()
-
-                    playSound(context!!, R.raw.tips)
-                    dismiss()
-                    updateFragment(fragment)
-                }
-            } else tipLetter.isEnabled = false
-            if (coins >= tipRemoveCost && levelString.last() != '!') {
-                tipRemove.setOnClickListener {
-                    fragment.letters.tipRemoveLetters(fragment.wordLetters)
-
-                    val editor = saves.edit()
-                    editor.putString(levelName, "$levelString!")
-                    editor.putInt("coins", coins - tipRemoveCost)
-                    editor.apply()
-
-                    playSound(context!!, R.raw.tips)
-                    dismiss()
-                    updateFragment(fragment)
-                }
-            } else tipRemove.isEnabled = false
-            if (coins >= tipSkipCost) {
-                tipSkip.setOnClickListener {
-                    val editor = saves.edit()
-                    editor.putInt("coins", coins - tipSkipCost)
-                    editor.apply()
-
-                    playSound(context!!, R.raw.tips)
-                    dismiss()
-                    updateFragment(fragment)
-                    fragment.win()
-                }
-            } else tipSkip.isEnabled = false
-        }
         builder.setView(view)
         val dialog = builder.create()
         dialog.window.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
