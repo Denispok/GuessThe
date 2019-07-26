73d34244cd3c40c086977d5759d137147e5a2dc4
denispok <shakhover@yandex.ru>
2019-07-26 14:36:54 +0700

    LevelSelection fix - add levelSelection color to resources

--- app/src/main/java/com/gamesbars/guessthe/customview/LevelView.kt
+++ app/src/main/java/com/gamesbars/guessthe/customview/LevelView.kt
@@ -10,8 +10,6 @@ class LevelView(context: Context) : FrameLayout(context) {
     init {
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         inflater.inflate(R.layout.item_levelselection, this, true)
-        val padding = context.resources.getDimension(R.dimen.item_levelselection_margin).toInt()
-        setPadding(padding, padding, padding, padding)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
